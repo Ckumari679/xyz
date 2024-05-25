59e4bb7cdb2a90332c313a7f8eb646e166f5a105
Ckumari679 <chhabikumari679@gmail.com>
2024-05-25 10:16:39 +0530

    pandas.py

---
+++ pandas test 1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().system('pip install pandas')
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+df=pd.read_csv('services (1).csv')
+df
+
+
+# ###### df
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
